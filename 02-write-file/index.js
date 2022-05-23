00ba45d4849feb93a07d280750586c2f33ad135c
AlexMingal <amingalev@gmail.com>
2022-05-24 00:06:23 +0200

    feat: fix 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
-
-const { dirname } = require('path');
-
 const path = require('path');
 
 const {stdin, stdout} = process;
 
 const out = fs.WriteStream(path.join(__dirname, 'data.txt'));
+
 
 stdout.write('Wake up Neo... enter something. (To quit type \'exit\' or press \'Ctrl + C\' ń\n');
 
